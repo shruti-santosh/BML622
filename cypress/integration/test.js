9f80bc6863a8b953bf959d36993a0f7bdf14390a
shruti-santosh <shrutisantosh@yahoo.com>
2024-10-20 21:17:50 +0530

    changed test file

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,13 +1,15 @@
 describe('Shruti Web Page Log Test', () => {
-    it('Should log "Page loaded successfully"', () => {
-      cy.visit('http://localhost:8080');
-  
-      // Capture console log
-      cy.window().then((win) => {
-        cy.stub(win.console, 'log').as('consoleLog');
-      });
-  
-      cy.get('@consoleLog').should('be.calledWith', 'Page loaded successfully: Shruti Santosh, 2023MCS310005');
+  it('Should log "Page loaded successfully"', () => {
+    // Visit the web page
+    cy.visit('shruti.html');
+
+    // Spy on the console log
+    const consoleLog = cy.spy(console, 'log');
+
+    // Assert that the log was called with the expected string
+    cy.wait(1000);  // Allow some time for the log to appear
+    cy.visit('shruti.html').then(() => {
+      expect(consoleLog).to.have.been.calledWith('Page loaded successfully: Shruti Santosh, 2023MCS310005');
     });
   });
-  
+});
